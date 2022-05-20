247d1cb9f7eae0272cae06efb086b7fb2487d9a2
flowercoder <36655532+flowercoder@users.noreply.github.com>
2022-05-20 22:57:28 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
-from setuptools import setup,find_packages
+from setuptools import setup ,find_packages
 setup(name='pytoolboxes',
       version='22.5.20',
       description='private python tool box',
       classifiers=[
-        'Programming Language :: Python',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-    ],
+          'Programming Language :: Python',
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+      ],
       url='https://github.com/flowercoder/pytoolboxes/',
       author='flowercoder',
-      author_email='',
+      author_email='gas1015@gmail.com',
       license='NEU',
       packages=find_packages(),
       zip_safe=True
-     )
+      )
