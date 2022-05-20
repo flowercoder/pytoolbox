ac8a701abdb518296c166c6b7ef42a17354a92f7
flowercoder <gas1015@gmail.com>
2022-05-21 00:59:32 +0800

    newfile

---
+++ pytoolboxes/toolworkwx.py
@@ -0,0 +1,9 @@
+from corpwechatbot import AppMsgSender
+
+
+class WorkWX():
+    def appMsgSender(self, corpid,corpsecret,agentid,content):
+        app = AppMsgSender(corpid=corpid,  # 你的企业id
+                           corpsecret=corpsecret,  # 你的应用凭证密钥
+                           agentid=agentid)  # 你的应用id
+        app.send_text(content)
