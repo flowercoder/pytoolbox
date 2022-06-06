8c42d318029eb849671326224c0b92420980dcc4
flowercoder <gas1015@gmail.com>
2022-06-06 13:53:05 +0800

    newfile

--- pytoolboxes/init.py
+++ pytoolboxes/init.py
@@ -1,11 +1,11 @@
 # check pytoolboxes
-
+import csv
 import os
 import configparser
 
 conf = configparser.ConfigParser() # 类的实例化
 curpath = os.path.dirname(os.path.realpath(__file__))
-path = os.path.join(curpath,'config.ini')
+path = os.path.join(curpath,'./config.ini')
 
 # conf.add_section('FLOWERCODER') # 添加一个新的section
 def renewpath(value):
@@ -15,6 +15,18 @@ def renewpath(value):
     conf.write(open(path,'w'))  # 保存数据
     print(path_value)
     return path
+
+def operatewrite(ips):
+    with open('./data.csv', 'w') as csvfile:
+        fieldnames = ['id', 'address']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+        # 写表头
+        writer.writeheader()
+        for i in range(len(ips)):
+            id = 10001 + i
+            # print(test[i])
+            writer.writerow({'id': id, 'address': ips[i]})
+
 
 def testpytoolboxes():#test toolbox
     return print("import pytoolboxes success")
